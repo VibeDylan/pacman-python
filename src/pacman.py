bdef537993b2dc111baca98bb9b369fe35b8849c
VibeDylan <dylanbourbotte62@icloud.com>
2025-12-11 15:46:42 +0100

    wip

--- src/pacman.py
+++ src/pacman.py
@@ -20,15 +20,30 @@ class Pacman(object):
         self.color = YELLOW
         self.node = node
         self.setPosition()
+        self.target = node
     
     def setPosition(self):
         self.position = self.node.position.copy()
         
     def update(self, dt):
+        self.position += self.directions[self.direction]*self.speed*dt
         direction = self.getValidKey()
-        self.direction = direction
-        self.node = self.getNewTarget(direction)
-        self.setPosition()
+            
+        if self.overshotTarget():
+            self.node = self.target
+            self.target = self.getNewTarget(direction)
+            if self.target is not self.node:
+                self.direction = direction
+
+            else:
+                self.target = self.getNewTarget(self.direction)
+
+            if self.target is self.node:
+                self.direction = STOP
+            self.setPosition()
+        else: 
+            if self.oppositeDirection(direction):
+                self.reverseDirection()
         
     def validDirection(self, direction):
         if direction is not STOP:
@@ -36,10 +51,31 @@ class Pacman(object):
                 return True
         return False
 
+    def overshotTarget(self):
+        if self.target is not None:
+            vec1 = self.target.position - self.node.position
+            vec2 = self.position - self.node.position
+            node2Target = vec1.magnitudeSquared()
+            node2Self = vec2.magnitudeSquared()
+            return node2Self >= node2Target
+        return False
+
     def getNewTarget(self, direction):
         if self.validDirection(direction):
             return self.node.neighbors[direction]
         return self.node
+    
+    def reverseDirection(self):
+        self.direction *= -1
+        temp = self.node
+        self.node = self.target
+        self.target = temp
+
+    def oppositeDirection(self, direction):
+        if direction is not STOP:
+            if direction == self.direction * -1:
+                return True
+        return False
         
     def getValidKey(self):
         key_pressed = pygame.key.get_pressed()
